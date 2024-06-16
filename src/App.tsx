b1af8f9428b6e63ed32940ecd59ddc65d8f2e369
大竹広明 <oahiroaki@gmail.com>
2024-06-16 21:44:39 +0900

    レイアウト直し

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
     }  
   }
   const [results, setResults] = useState(defaultResults)
-  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
   const inputImageRef = useRef<HTMLInputElement>(null)
 
   function toString(value: number): string {
@@ -56,10 +56,10 @@ function App() {
 
 
   async function loadStatusFromImage() {
-    setButtonDisabled(true)
+    setLoading(true)
 
     if (inputImageRef.current == null || inputImageRef.current.files == null || inputImageRef.current.files.length === 0) {
-      setButtonDisabled(false)
+      setLoading(false)
       return
     }
 
@@ -105,11 +105,10 @@ function App() {
             }    
           })
         }
-        // OCRで "1234" のような値を読み取っているところをマッチさせる
       })
 
       if (positionList.length === 0 || statsList.length === 0) {
-        setButtonDisabled(false)
+        setLoading(false)
         return
       }
     
@@ -148,18 +147,19 @@ function App() {
         setVisual(results.visual.toString(10))
       }
 
-      setButtonDisabled(false)
+      setLoading(false)
     } catch (err) {
       console.error(err)
+      setLoading(false)
     }
   }
 
   return (
-    <div className="max-w-[800px] p-2 m-auto">
-      <main className="w-full flex flex-wrap">
+    <div className="md:max-w-[768px] p-2 m-auto">
+      <main className="w-full">
         <div className="">
           <div className="my-2 font-bold">最終試験開始時のステータス</div>
-          <div className="w-full flex gap-4">
+          <div className="w-full flex flex-wrap md:gap-4">
             <StatusInputComponent label="ボーカル" value={vocal} setValue={setVocal} type="vocal" />
             <StatusInputComponent label="ダンス" value={dance} setValue={setDance} type="dance" />
             <StatusInputComponent label="ビジュアル" value={visual} setValue={setVisual} type="visual" />
@@ -197,18 +197,29 @@ function App() {
               下のような最終試験前のスクリーンショットを選択して、スクショから読み込むボタンを押してください。<br/>
               うまく読み込めないときもあります。
             </div>
-            <div className="flex gap-4">
-              <img src={sampleImage} className="w-28" />
+            <div className="flex gap-4 flex-wrap">
+              <img src={sampleImage} className="w-28 ml-4" />
               <div className="flex flex-col gap-4">
                 <input type="file" accept="image/*" ref={inputImageRef} />
                 <div>
                   <button
-                    className="border-2 border-green-200 bg-green-100 hover:opacity-80 rounded px-3 py-1"
+                    className="border-2 border-gray-200 bg-gray-100 hover:opacity-80 rounded w-44 h-8 flex items-center justify-center"
                     onClick={() => loadStatusFromImage()}
-                    disabled={buttonDisabled}
+                    disabled={loading}
                   >
-                    スクショから読み込む
+                    {loading ? (
+                      <div role="status">
+                        <svg aria-hidden="true" className="w-6 h-6 text-gray-200 animate-spin fill-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                      </div>
+                    ) : (
+                      <div>スクショから読み込む</div>
+                    )}
                   </button>
+
                 </div>
               </div>
             </div>
@@ -225,9 +236,9 @@ function App() {
             <table className="table-auto">
               <thead className="bg-gray-100">
                 <tr>
-                  <th className="px-4 py-2 whitespace-nowrap">ランク</th>
-                  <th className="px-4 py-2 whitespace-nowrap">必要な最終試験の点数</th>
-                  <th className="px-4 py-2 whitespace-nowrap">不足している点数</th>
+                  <th className="px-4 py-2 whitespace-nowrap text-xs md:text-base">ランク</th>
+                  <th className="px-4 py-2 whitespace-nowrap text-xs md:text-base">必要な最終試験の点数</th>
+                  <th className="px-4 py-2 whitespace-nowrap text-xs md:text-base">不足している点数</th>
                 </tr>
               </thead>
               <tbody>
@@ -252,10 +263,6 @@ function App() {
           </div>
         </div>
       </main>
-      <footer>
-        <div className="">
-        </div>
-      </footer>
     </div>
   )
 }
